83c5c4de3019f7d18f40e84b611bd01175758877
Tong <tongmeanlove15@gmail.com>
2022-09-22 11:46:30 +0700

    Deploy Check

--- Auth/settings.py
+++ Auth/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hf$@#pr@+)(e^g-2l!_km@fhz05!24hl4!lacs5!wo+b(tqdr5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
